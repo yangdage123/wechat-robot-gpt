42dc27a4760bad394e30275fb29cd9c9cc88ce0a
老杨头 <515411030@qq.com>
2023-03-16 21:23:13 +0800

    feature：修复问题

--- module/handle-message.js
+++ module/handle-message.js
@@ -1,5 +1,22 @@
+import { getWeather } from './sendrequest.js';
+import cityGeo from '../data/city-geo.json' assert { type: 'json' };
 
+const location = '南京市';
+const appid = '';
 
+const main = async () => {
+    const r = cityGeo.filter((v) => (v.area === location || (v.city === '市辖区' ? v.province === location : v.city === location)));
+    if (r.length >= 1) {
+        const { lat, lon } = r[0];
+        const response = await getWeather({ lat, lon, appid });
+        if (response.status === 200) {
+            response.data.daily.filter(v => v.temp.day)
+        }
+    }
+}
+main();
+
+/*
 export const handleMessage = async (message, bot) => {
     const contact = message.talker();
     const msgType = message.type();
@@ -9,5 +26,4 @@ export const handleMessage = async (message, bot) => {
             await message.say('今天的天气不错哦!')
         }
     }
-
-}
+}*/
--- module/sendrequest.js
+++ module/sendrequest.js
@@ -1,6 +1,3 @@
-import { post } from 'axios';
+import axios from 'axios';
 
-
-export const getWeather = post('', {
-
-});
+export const getWeather = ({ lat, lon, appid }) => axios.get(`https://api.openweathermap.org/data/2.5/onecall?exclude=minutely,hourly,alerts&lang=zh_cn&units=metric&lat=${lat}&lon=${lon}&appid=${appid}`);
